94b8be692259419dea000a4ce9c56b5903922b06
Farisnaufal10 <farisnaufal100404&gmail.com>
2026-04-21 16:01:30 +0700

    DEV-44: feat: implementasi upload foto profil menggunakan multer

---
+++ Backend/src/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const profileRoutes = require('./routes/profileRoutes');
+
+app.use(express.json());
+app.use('/api/profiles', profileRoutes);
+
+app.listen(4002, () => {
+    console.log('Profile Service running on port 4002');
+});
