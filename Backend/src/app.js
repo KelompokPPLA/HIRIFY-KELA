7bf0338139c40a2ca955998852700fad39025ba9
Farisnaufal10 <farisnaufal100404&gmail.com>
2026-04-22 08:56:33 +0700

    DEV-2: feat: implementasi upload CV dengan validasi file PDF

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
+
 const profileRoutes = require('./routes/profileRoutes');
+const cvRoutes = require('./routes/cvRoutes');
 
 app.use(express.json());
-app.use('/api/profiles', profileRoutes);
 
-app.listen(4002, () => {
-    console.log('Profile Service running on port 4002');
+app.use('/api/profiles', profileRoutes);
+app.use('/api/cvs', cvRoutes);
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
 });
