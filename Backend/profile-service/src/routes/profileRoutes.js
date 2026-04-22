7f439cd322a832c55fc770c994d58455d54c8094
Farisnaufal10 <farisnaufal100404&gmail.com>
2026-04-22 15:42:20 +0700

    DEV-44: menambahkan fitur upload foto profile

--- Backend/profile-service/src/routes/profileRoutes.js
+++ Backend/profile-service/src/routes/profileRoutes.js
@@ -7,8 +7,12 @@ router.post('/', controller.createProfile);
 router.get('/:userId', controller.getProfile);
 router.put('/:userId', controller.updateProfile);
 
-// upload foto
+// upload foto profil
 router.post('/upload/:userId', upload.single('photo'), (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({ message: 'Foto wajib diupload' });
+    }
+
     res.json({
         message: 'Foto berhasil diupload',
         file: req.file.filename
