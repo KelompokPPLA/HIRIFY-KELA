e293aa08109a04527e6be3b8af7741976e0a3436
Achmad Vickram Ritonga <148619438+AHVIRI@users.noreply.github.com>
2026-04-23 20:54:37 +0700

    Menghapus folder backend

--- Backend/cv-service/src/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const cvRoutes = require('./routes/cvRoutes');
-
-app.use(express.json());
-app.use('/api/cvs', cvRoutes);
-app.listen(4003, () => {
-    console.log('CV Service running on port 4003');
-});
