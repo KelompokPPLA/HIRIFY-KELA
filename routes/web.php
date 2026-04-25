5b5834877da2406a2d49c867277eaf044ac948f1
Farisnaufal10 <farisnaufal100404&gmail.com>
2026-04-25 13:37:29 +0700

    DEV-1: menambahkan routing untuk halaman profil

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
 });
