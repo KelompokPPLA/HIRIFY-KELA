aba91af8ecb2ebb89028637414066bd5779624c7
Farisnaufal10 <farisnaufal100404&gmail.com>
2026-04-25 15:32:20 +0700

    DEV-1: Mengubah isi routes tanpa auth untuk menjalankan tampilan profil tanpa login dulu

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-
-Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
     Route::post('/profile', [ProfileController::class, 'store'])->name('profile.store');
     Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
-});
+
