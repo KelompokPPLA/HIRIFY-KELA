2d1ec8d561da6bc3cf8b3ace0698e4fdc4f1ae5f
AHVIRI <achmadvickramritonga0@gmail.com>
2026-04-26 05:50:18 +0700

    DEV-9 Melakukan perubahan pada route dan database untuk fitur mentorship

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MentorCertificationController;
+use App\Http\Controllers\MentorshipController;
 use App\Http\Controllers\MentorProfileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
@@ -33,4 +34,13 @@ Route::middleware(['auth:api', 'role:mentor'])->prefix('mentor')->group(function
 	Route::get('certifications', [MentorCertificationController::class, 'index']);
 	Route::post('certifications', [MentorCertificationController::class, 'store']);
 	Route::delete('certifications/{id}', [MentorCertificationController::class, 'destroy']);
+});
+
+Route::middleware(['auth:api', 'role:jobseeker'])->prefix('mentorship')->group(function () {
+	Route::get('mentors', [MentorshipController::class, 'mentors']);
+	Route::get('mentors/{id}', [MentorshipController::class, 'mentorDetail']);
+	Route::get('mentors/{id}/availability', [MentorshipController::class, 'mentorAvailability']);
+	Route::post('bookings', [MentorshipController::class, 'createBooking']);
+	Route::get('bookings/my', [MentorshipController::class, 'myBookings']);
+	Route::patch('bookings/{id}/cancel', [MentorshipController::class, 'cancelBooking']);
 });
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,4 @@ Route::view('/forgot-password', 'auth.forgot-password')->name('password.request'
 Route::view('/reset-password', 'auth.reset-password')->name('password.reset');
 Route::view('/dashboard', 'auth.dashboard')->name('dashboard');
 Route::view('/mentor/settings', 'mentor.settings')->name('mentor.settings');
+Route::view('/mentorship', 'jobseeker.mentorship')->name('mentorship.index');
