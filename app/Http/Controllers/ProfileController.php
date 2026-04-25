164ad5797bf318423dc4ff36df367c9f45d7652d
Farisnaufal10 <farisnaufal100404&gmail.com>
2026-04-25 13:35:02 +0700

    DEV-1: menambahkan method index untuk menampilkan data profil user

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
-    
+    public function index()
+    {
+        $profile = Auth::user()->profile;
+        return view('profile.index', compact('profile'));
+    }
 }
