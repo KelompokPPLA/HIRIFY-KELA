08452d959d27c23f64d7f13d45d801a1e0f9e6b0
Farisnaufal10 <farisnaufal100404&gmail.com>
2026-04-25 13:57:18 +0700

    DEV-1: menambahkan routing untuk create dan update profil

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,6 @@ use App\Http\Controllers\ProfileController;
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
+    Route::post('/profile', [ProfileController::class, 'store'])->name('profile.store');
+    Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
 });
