07f370e24a4df904e1a4f4b31083438159e29101
l4mus_ <muhnazli6804@gmail.com>
2026-04-27 15:19:15 +0700

    Delete Backend directory

--- Backend/realtime-service/src/config/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  port: process.env.REALTIME_SERVICE_PORT || 4010,
-  service: 'realtime-service',
-};
--- Backend/roadmap-service/src/config/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  port: process.env.ROADMAP_SERVICE_PORT || 4004,
-  service: 'roadmap-service',
-};
