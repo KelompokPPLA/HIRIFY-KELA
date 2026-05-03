d4b0408030c8f5bf42ed0e98bb42fb08b266c097
Abrar <abrarrayhan8@gmail.com>
2026-05-03 15:19:43 +0700

    DEV-8: perbarui deskripsi card Progress Pelatihan agar menampilkan persentase progres keseluruhan jika masih ada kursus yang belum selesai

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -51,7 +51,9 @@
                     'label' => 'Progress Pelatihan',
                     'value' => $trainingProgress . '%',
                     'progress' => $trainingProgress,
-                    'desc' => $trainingTotal === 0 ? 'Belum ada pelatihan terdaftar.' : ($trainingCompleted . ' dari ' . $trainingTotal . ' pelatihan selesai.'),
+                    'desc' => $trainingTotal === 0
+                        ? 'Belum ada pelatihan terdaftar. Mulai belajar!'
+                        : ($trainingCompleted . ' dari ' . $trainingTotal . ' kursus selesai' . ($trainingProgress > 0 && $trainingCompleted < $trainingTotal ? ' · ' . $trainingProgress . '% progres keseluruhan' : '') . '.'),
                     'icon' => '<path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>',
                 ],
                 [
